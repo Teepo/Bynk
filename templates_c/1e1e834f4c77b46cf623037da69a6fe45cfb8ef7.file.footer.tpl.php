3dc0eaf28be603a1b25a70411f26af992d8d835c
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2015-03-21 03:15:10 +0100

    Add smarty cache.

---
+++ templates_c/1e1e834f4c77b46cf623037da69a6fe45cfb8ef7.file.footer.tpl.php
@@ -0,0 +1,23 @@
+<?php /* Smarty version Smarty-3.1.14, created on 2015-03-01 19:02:06
+         compiled from "views/footer.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:212522409054f3541e401c59-03801786%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '1e1e834f4c77b46cf623037da69a6fe45cfb8ef7' => 
+    array (
+      0 => 'views/footer.tpl',
+      1 => 1425230851,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '212522409054f3541e401c59-03801786',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.14',
+  'unifunc' => 'content_54f3541e4068a4_68156467',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_54f3541e4068a4_68156467')) {function content_54f3541e4068a4_68156467($_smarty_tpl) {?></body></html>
+<?php }} ?>
