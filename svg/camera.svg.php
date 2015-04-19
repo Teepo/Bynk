bde58a19fe25f76b7a4b37fac5f45c6cfe3d052d
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2015-04-19 19:39:22 +0200

    SVG: add pen & camera.

---
+++ svg/camera.svg.php
@@ -0,0 +1,22 @@
+<svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+     width="112px" height="112px" viewBox="0 0 112 112" style="enable-background:new 0 0 112 112;" xml:space="preserve">
+  <g <?php self::_svg_fill("FFF") ?>>
+    <path d="M10.389,23.333h68.945v65.334H10.389C4.652,88.667,0,84.015,0,78.276V33.721C0,27.986,4.652,23.333,10.389,23.333z
+		     M106.396,28L84,42v28l22.396,14H112V28H106.396z"/>
+  </g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+  <g></g>
+</svg>
