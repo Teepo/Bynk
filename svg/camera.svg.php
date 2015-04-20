f70d56b5a5d8dcdb84a2b526a057f576222b5132
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2015-04-20 23:55:03 +0200

    SVG: add some file.

--- svg/camera.svg.php
+++ svg/camera.svg.php
@@ -1,22 +1,9 @@
 <svg version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-     width="112px" height="112px" viewBox="0 0 112 112" style="enable-background:new 0 0 112 112;" xml:space="preserve">
-  <g <?php self::_svg_fill("FFF") ?>>
-    <path d="M10.389,23.333h68.945v65.334H10.389C4.652,88.667,0,84.015,0,78.276V33.721C0,27.986,4.652,23.333,10.389,23.333z
-		     M106.396,28L84,42v28l22.396,14H112V28H106.396z"/>
+     width="32px" height="32px" viewBox="0 0 512 512" enable-background="new 0 0 512 512" xml:space="preserve">
+  <g fill="none" fill-rule="evenodd" stroke="none" stroke-width="1">
+    <g <?php self::_svg_fill('FFF') ?>>
+      <path d="M319,317.084c0,20.94-16.976,37.916-37.916,37.916H87.916C66.976,355,50,338.024,50,317.084V194.916
+	           C50,173.976,66.976,157,87.916,157h193.168c20.94,0,37.916,16.976,37.916,37.916V317.084z M462,157l-113,68.5v61L462,355V157z"/>
+    </g>
   </g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
-  <g></g>
 </svg>
