8dbbbfa222461d41d3be3650ddcf38e1f5ba5218
Anthony Ribeiro <anthony.ribeiro77@gmail.com>
2015-04-23 23:07:08 +0200

    JS: form > add autocomplete listener.

---
+++ js/tools/form.js
@@ -0,0 +1,37 @@
+(function(){
+
+    var form = {};
+
+    var AUTOCOMPLETE_MIN_CHAR = 3;
+
+    form.init = function () {
+
+        EVENT.add(document.querySelectorAll('form[autocomplete] input'), 'keyup', form.autocomplete);
+    };
+
+    form.autocomplete = function(event) {
+
+        var input = event.target;
+
+        if (input.value.length < AUTOCOMPLETE_MIN_CHAR)
+            return false;
+
+        var _form = closest(input, 'form')
+        var section = closest(_form, 'section');
+        var url = _form.getAttribute('action') + "argv/name/" + input.value;
+
+        XHR.get(url, function(response) {
+            var handler = section.querySelector('.handler');
+
+            handler.innerHTML = response;
+
+            EVENT.triggerCustom(document, 'autocomplete', {
+                target: _form
+            });
+        });
+    };
+
+    document.addEventListener('DOMContentLoaded', function() {
+        form.init();
+    });
+})();
